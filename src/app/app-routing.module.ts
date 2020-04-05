6076126bfd620b554de001ea36537a12c418a16c
KS <roatus@gmail.com>
2020-04-05 20:01:26 +0900

    2020/04/05 20:01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ZipcloudComponent } from './zipcloud/zipcloud.component';
+import { RandomuserComponent } from './randomuser/randomuser.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'zipcloud', component: ZipcloudComponent },
+  {path: 'randomuser', component: RandomuserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
