e20e7c778eef27762830f40f0c0845758bece812
KS <roatus@gmail.com>
2020-04-29 23:04:08 +0900

    2020/04/29 23:03 Committing before testing Authentication.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ZipcloudComponent } from './zipcloud/zipcloud.component';
 import { RandomuserComponent } from './randomuser/randomuser.component';
+import { LocalProductComponent } from './local-product/local-product.component';
 
 const routes: Routes = [
   {path: 'zipcloud', component: ZipcloudComponent },
   {path: 'randomuser', component: RandomuserComponent},
+  {path: 'local-product', component: LocalProductComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ZipcloudComponent } from './zipcloud/zipcloud.component';
 import { RandomuserComponent } from './randomuser/randomuser.component';
+import { LocalProductComponent } from './local-product/local-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     ZipcloudComponent,
-    RandomuserComponent    
+    RandomuserComponent,
+    LocalProductComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
